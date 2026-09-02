89840e882eda90ccc3823d4e3396bb58a2dc418b
adithor001-byte <adithor001@gmail.com>
2026-09-03 00:28:42 +0530

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       vector<char>v1;
+       vector<char>v2;
+       if(s.length()!=t.length())
+       return false;
+       else{
+       for(int i=0;i<s.length();i++)
+       {
+        v1.push_back(s[i]);
+       }
+        for(int i=0;i<t.length();i++)
+       {
+        v2.push_back(t[i]);
+       }
+       sort(v1.begin(),v1.end());
+       sort(v2.begin(),v2.end());
+       
+        if(v1!=v2)
+        {
+            return false;
+        }
+       
+       return true;
+    }
+    }
+};
