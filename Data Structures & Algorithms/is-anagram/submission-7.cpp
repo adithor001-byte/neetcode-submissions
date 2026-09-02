26588af64e8a9422b9d45b0e503bf86f5bf29610
adithor001-byte <adithor001@gmail.com>
2026-09-03 00:27:42 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+        return false;
+        else{
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;
+        for(auto i:s)
+        {
+            mp1[i]++;
+        }
+        for(auto i:t)
+        {
+            mp2[i]++;
+        }
+       if(mp1!=mp2)
+       {
+        return false;
+       }
+        return true;
+        }
+    }
+};
