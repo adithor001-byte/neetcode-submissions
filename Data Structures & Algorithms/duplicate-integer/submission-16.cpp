0784a3f288090340fe4517f702fb3c5a85bd34f5
adithor001-byte <adithor001@gmail.com>
2026-09-18 23:34:40 +0530

    Add: duplicate-integer - submission-16

---
+++ Data Structures & Algorithms/duplicate-integer/submission-16.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(auto i:nums)
+        {
+            if(mp.find(i)!=mp.end())
+            {
+                return true;
+            }
+            else
+            {
+                mp[i]++;
+            }
+            
+        }
+        return false;
+    }
+};
